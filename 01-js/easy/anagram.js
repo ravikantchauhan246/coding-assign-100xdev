d69a8a5f8caa188821118e0ce97605be4192968f
ravikantchauhan246 <ravikantchauhan246@gmail.com>
2024-05-29 14:08:50 +0530

    Done with 1 and 3 easy

--- 01-js/easy/anagram.js
+++ 01-js/easy/anagram.js
@@ -5,7 +5,19 @@
 */
 
 function isAnagram(str1, str2) {
-        
+  if(str1.length!==str2.length){
+    return false;
+  }
+  
+  const cleanStr1 = str1.replace(/[]/g, '').toLowerCase();
+  const cleanStr2 = str2.replace(/[]/g, '').toLowerCase();
+
+  // Sort the characters in each string
+  const sortedStr1 = cleanStr1.split('').sort().join('');
+  const sortedStr2 = cleanStr2.split('').sort().join('');
+
+  // Compare the sorted strings
+  return sortedStr1 === sortedStr2;
 }
 
 module.exports = isAnagram;
